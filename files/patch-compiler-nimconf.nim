c7fe27ebfc912ebc7bbeb76e943c814ec6172088
Leynos <leynos@troubledskies.net>
2015-03-13 23:39:03 +0000

    nimconf patch updated

--- files/patch-compiler-nimconf.nim
+++ files/patch-compiler-nimconf.nim
@@ -1,22 +1,26 @@
---- compiler/nimconf.nim.orig
-+++ compiler/nimconf.nim
-@@ -213,7 +213,9 @@
- proc getSystemConfigPath(filename: string): string =
+--- compiler/nimconf.nim.orig	2014-11-23 15:24:36.000000000 +0000
++++ compiler/nimconf.nim	2015-03-13 23:21:20.638219323 +0000
+@@ -214,21 +214,17 @@
    # try standard configuration file (installation did not distribute files
    # the UNIX way)
--  result = joinPath([getPrefixDir(), "config", filename])
-+  var prefix = getPrefixDir()
-+  result = joinPath([prefix, "etc", filename])
-+  if not ExistsFile(result): result = joinPath([prefix, "config", filename])
-   if not ExistsFile(result): result = "/etc/" & filename
+   let p = getPrefixDir()
+-  result = joinPath([p, "config", filename])
+-  when defined(unix):
+-    if not existsFile(result): result = joinPath([p, "etc", filename])
+-    if not existsFile(result): result = "/etc/" & filename
++  result = joinPath([p, "etc", filename])
++  if not existsFile(result): result = joinPath([p, "config", filename])
++  if not existsFile(result): result = "/etc/" & filename
  
- proc LoadConfigs*(cfg: string) =
-@@ -221,9 +223,8 @@
+ proc loadConfigs*(cfg: string) =
+   # set default value (can be overwritten):
    if libpath == "": 
      # choose default libpath:
      var prefix = getPrefixDir()
--    if prefix == "/usr": libpath = "/usr/lib/nimrod"
--    elif prefix == "/usr/local": libpath = "/usr/local/lib/nimrod"
+-    when defined(posix):
+-      if prefix == "/usr": libpath = "/usr/lib/nim"
+-      elif prefix == "/usr/local": libpath = "/usr/local/lib/nim"
+-      else: libpath = joinPath(prefix, "lib")
 -    else: libpath = joinPath(prefix, "lib")
 +    libpath = joinPath(prefix, "lib/nimrod")
 +    if not ExistsDir(libpath): libpath = joinPath(prefix, "lib")

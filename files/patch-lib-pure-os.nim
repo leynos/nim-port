eb4ba43897633337cda54da9ff21285640ac643f
Leynos <leynos@troubledskies.net>
2015-03-14 00:03:10 +0000

    New patch for pure os lib

--- files/patch-lib-pure-os.nim
+++ files/patch-lib-pure-os.nim
@@ -1,36 +1,12 @@
---- lib/pure/os.nim.orig
-+++ lib/pure/os.nim
-@@ -1389,8 +1389,6 @@
-     result = getApplAux("/proc/self/exe")
-   elif defined(solaris):
+--- work/nim-0.10.2/lib/pure/os.nim.orig	2015-03-14 00:02:20.402048491 +0000
++++ work/nim-0.10.2/lib/pure/os.nim	2015-03-14 00:02:34.596048295 +0000
+@@ -1785,8 +1785,7 @@
      result = getApplAux("/proc/" & $getpid() & "/path/a.out")
--  elif defined(freebsd):
+     if result.len == 0: result = getApplHeuristic()
+   elif defined(freebsd):
 -    result = getApplAux("/proc/" & $getpid() & "/file")
+-    if result.len == 0: result = getApplHeuristic()
++    result = getApplHeuristic()
    elif defined(macosx):
      var size: cuint32
      getExecPath1(nil, size)
-@@ -1399,16 +1397,14 @@
-       result = "" # error!
-   else:
-     # little heuristic that may work on other POSIX-like systems:
--    result = string(getEnv("_"))
--    if len(result) == 0:
--      result = string(ParamStr(0))
--      # POSIX guaranties that this contains the executable
--      # as it has been executed by the calling process
--      if len(result) > 0 and result[0] != DirSep: # not an absolute path?
--        # iterate over any path in the $PATH environment variable
--        for p in split(string(getEnv("PATH")), {PathSep}):
--          var x = joinPath(p, result)
--          if ExistsFile(x): return x
-+    result = string(ParamStr(0))
-+    # POSIX guaranties that this contains the executable
-+    # as it has been executed by the calling process
-+    if len(result) > 0 and result[0] != DirSep: # not an absolute path?
-+      # iterate over any path in the $PATH environment variable
-+      for p in split(string(getEnv("PATH")), {PathSep}):
-+        var x = joinPath(p, result)
-+        if ExistsFile(x): return x
- 
- proc getApplicationFilename*(): string {.rtl, extern: "nos$1", deprecated.} =
-   ## Returns the filename of the application's executable.
